38a8f95595bd99cbdbfc3e7fb82214e001ed0d40
Guilherme Arimatea <rm365138@fiap.com.br>
2025-07-28 22:28:53 -0300

    feature: added first entities and seed function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { ContactInfo } from './entities/contact-info.entity';
+import { Employee } from './entities/employee.entity';
+import { Meeting } from './entities/meeting.entity';
+import { Task } from './entities/task.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: true,
+      logging: false,
+    }),
+    TypeOrmModule.forFeature([Employee, ContactInfo, Task, Meeting]),
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
